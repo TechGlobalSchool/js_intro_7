1fafb2f25ae2e112553f61844da908ce4d9be577
TechGlobalSchool <tgschool01@gmail.com>
2023-08-05 11:47:45 -0500

    added more string methods

--- js_basics/string_functions_02.js
+++ js_basics/string_functions_02.js
@@ -41,3 +41,55 @@ console.log(quote.includes(quote)); // true
 console.log(quote.includes("")); // true
 console.log(quote.includes(" ")); // true
 console.log(quote.includes("  ")); // false
+
+
+
+// indexOf() vs lastIndexOf()
+let greeting = "GOOD MORNING";
+console.log(greeting.indexOf("G")); // 0
+console.log(greeting.lastIndexOf("G")); // 11
+
+console.log(greeting.indexOf("GOOD")); // 0
+console.log(greeting.lastIndexOf("GOOD")); // 0
+console.log(greeting.indexOf("GOOD MORNING")); // 0
+console.log(greeting.lastIndexOf("GOOD MORNING")); // 0
+
+let sentence = "I like apple and pineapple";
+console.log(sentence.indexOf("apple")); // 7
+console.log(sentence.lastIndexOf("apple")); // 21
+
+// TRICKY PART
+console.log(sentence.indexOf("abc")); // -1
+console.log(sentence.lastIndexOf("abc")); // -1
+
+console.log(sentence.indexOf(" ")); // 1
+console.log(sentence.lastIndexOf(" ")); // 16
+
+console.log(sentence.indexOf("")); // 0
+console.log(sentence.lastIndexOf("")); // 26 
+
+
+
+// startsWith() vs endsWith()
+let info = "My phone is 312-000-0000";
+
+let b1 = info.startsWith("M");
+let b2 = info.startsWith("My");
+let b3 = info.endsWith("0000"); 
+
+console.log(b1); // true
+console.log(b2); // true
+console.log(b3); // true
+
+console.log(info.startsWith("phone")); // false
+console.log(info.endsWith("0")); // true
+
+// TRICKY PART
+console.log(info.startsWith(info)); // true
+console.log(info.endsWith(info)); // true
+console.log(info.startsWith("")); // true
+console.log(info.endsWith("")); // true
+
+
+
+
