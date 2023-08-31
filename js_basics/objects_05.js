21c36effe9ebb5c0ef1ed5a6ca4f533988112ace
TechGlobalSchool <tgschool01@gmail.com>
2023-08-30 20:59:03 -0500

    added more practices

--- js_basics/objects_05.js
+++ js_basics/objects_05.js
@@ -27,23 +27,32 @@ const appleStore = [
 ];
 
 // Find the most expensive product   -> MacBook Pro 16-inch
-
 // Find the least expensive product   -> AirPods Pro
-
 // Find the product with the biggest quantity   -> AirPods Pro
-
 // Find the product with the smallest quantity  -> MacBook Pro 16-inch
-
 // Find all the product names   -> [ 'iPhone 14 Pro', 'MacBook Pro 16-inch', 'iPad Air', 'Apple Watch Series 7', 'AirPods Pro' ]
 
-let most_expensive = {
-    productName: null,
-    price: 0,
-    quantity: 0
-};
-
-for(const product of appleStore){
-    if(product.price > most_expensive.price) most_expensive = product;
+if (appleStore.length === 0) {
+    console.log('This store is empty! NO PRODUCT')
 }
+else {
+    let most_expensive = appleStore[0];
+    let most_affordable = appleStore[0];
+    let biggest_quantity = appleStore[0];
+    let lowest_quantity = appleStore[0];
+    const all_product_names = [];
 
-console.log(most_expensive.productName);
+    for (const product of appleStore) {
+        if (product.price > most_expensive.price) most_expensive = product;
+        if (product.price < most_affordable.price) most_affordable = product;
+        if (product.quantity > biggest_quantity.quantity) biggest_quantity = product;
+        if (product.quantity < lowest_quantity.quantity) lowest_quantity = product;
+        all_product_names.push(product.productName);
+    }
+
+    console.log(most_expensive.productName);
+    console.log(most_affordable.productName);
+    console.log(biggest_quantity.productName);
+    console.log(lowest_quantity.productName);
+    console.log(all_product_names)
+}
