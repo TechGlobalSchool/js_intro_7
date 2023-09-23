91e38f90de3d4e25b855b15d202319ba48ec92c3
TechGlobalSchool <tgschool01@gmail.com>
2023-09-23 11:59:23 -0500

    DOM day 3

--- js_basics/DOM/dom4.js
+++ js_basics/DOM/dom4.js
@@ -2,6 +2,9 @@
  * DOM Events
  */
 
+// addEventListener
+// element.addEventListener('event type', callback);
+
 /**
  * 1. Mouse events
  */
@@ -57,21 +60,105 @@ mouseOutBtn.addEventListener('mouseout', (event) => {
 const body = document.querySelector('body');
 
 // keydown
-body.addEventListener('keydown', (event) => {
-    // if(event.key === 13) {
-        console.log('Keydown event => ', event.target);
-    // }
+// body.addEventListener('keydown', (event) => {
+//     // if(event.key === 13) {
+//         console.log('Keydown event => ', event.target);
+//     // }
+// });
+
+// // keyup
+// body.addEventListener('keyup', (event) => {
+//         console.log('Keyup event => ', event.target);
+// });
+
+// // keypress
+// body.addEventListener('keypress', (event) => {
+//     console.log('Keypress event => ', event.target);
+// });
+
+/**
+ * Form events
+ */
+
+// change
+const changeInput = document.getElementById('changeInput');
+
+changeInput.addEventListener('change', (event) => {
+    // event.target.value => whatever user types on input 
+    console.log(event.target.value);
 });
 
-// keyup
-body.addEventListener('keyup', (event) => {
-        console.log('Keyup event => ', event.target);
+
+// input
+const inputInput = document.getElementById('inputInput');
+
+inputInput.addEventListener('input', (event) => {
+    console.log(event.target.value);
 });
+
+// focus
+const formInput = document.getElementById('formInput');
+
+formInput.addEventListener('focus', (event) => {
+    let element =  event.target;
+    element.style.backgroundColor = 'darkred';
+    element.style.color = 'white';
+})
+// blur
+formInput.addEventListener('blur', (event) => {
+    let element =  event.target;
+    element.style.backgroundColor = '';
+    element.style.color = '';
+})
+
+// submit
+const formEl = document.querySelector('form');
+
+// Uncomment button element for this to work
+// formEl.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     console.log('Form successfully submitted!');
+//     console.log({
+//         name: 'Joe',
+//         email: 'joe@gmail.com'
+//     });
+// })
+
 
 // keypress
-body.addEventListener('keypress', (event) => {
-    console.log('Keypress event => ', event.target);
+formEl.addEventListener('keypress', (event) => {
+    event.preventDefault();
+    if(event.code === 13) {
+        console.log('Form successfully submitted!');
+        console.log({
+            name: 'Joe',
+            email: 'joe@gmail.com'
+        });
+    }
+   
+})
+
+/**
+ * 4. Window event
+ */
+
+// scroll
+window.addEventListener('scroll', () => {
+    console.log('You are scrolling the page!')
+    console.log(`And your coordinates are X: ${window.scrollX} and Y: ${window.scrollY} `);
 });
+
+
+/*
+   Activity: Todo list
+
+    Step 1: Create an input to take a list
+
+    Step 2: Either keypress or submit 
+
+    Step 3: Show that list in the UI
+
+*/
 
 
 
