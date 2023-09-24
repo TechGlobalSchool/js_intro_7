91e623b3d4c76fe510b02518052725d2ec0c8441
TechGlobalSchool <tgschool01@gmail.com>
2023-09-24 12:21:26 -0500

    modules part 3

---
+++ js_basics/Modules/Example3/date.js
@@ -0,0 +1,11 @@
+export const getCurrentYear = () => {
+    return new Date().getFullYear();
+}
+
+
+export const add = (a,b) => {
+    console.log('add from date.js');
+    return a + b;
+}
+
+
---
+++ js_basics/Modules/Example4/helpers/index.js
@@ -0,0 +1,12 @@
+// export { add, substract } from './math.js';
+// export { getCurrentYear } from './date.js';
+
+// Below syntax is same as above
+import { add, substract } from './math.js';
+import { getCurrentYear } from './date.js';
+
+export {
+    add,
+    substract,
+    getCurrentYear
+}
